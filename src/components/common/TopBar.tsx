47c6dbecf50cbe62e7756ae3376017c98ed07e95
Yoshida Takuto <takuto.yoshida@gmail.com>
2020-06-07 01:19:59 +0900

    TopBarのStyleを追加

--- src/components/common/TopBar.tsx
+++ src/components/common/TopBar.tsx
@@ -1,19 +1,34 @@
 import React from 'react'
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
 import { Link, useLocation } from 'react-router-dom'
 import TopBarLink from './TopBarLink'
+
+import styles from './TopBar.scss'
 
 const TopBar: React.FC = () => {
   const { pathname } = useLocation()
 
+  const username = 'username'
+
   return (
-    <Navbar bg='light' expand='lg'>
+    <Navbar variant='dark' expand='lg' className={styles.self}>
       <Navbar.Brand as={Link} to='/'>
         Mikan
       </Navbar.Brand>
+      <Navbar.Toggle />
       <Navbar.Collapse>
         <Nav className='mr-auto' activeKey={`#${pathname}`}>
           <TopBarLink to='/'>Home</TopBarLink>
+        </Nav>
+        <Nav className='ml-auto'>
+          <NavDropdown id='nav-dropdown' title={username}>
+            <NavDropdown.Item disabled as={Link} to='/profile'>
+              Profile
+            </NavDropdown.Item>
+            <NavDropdown.Item as={Link} to='/logout'>
+              Logout
+            </NavDropdown.Item>
+          </NavDropdown>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { render } from 'react-dom'
 import App from './App'
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './index.scss'
 
 const Element = React.createElement(App)
 
